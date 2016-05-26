ef2f3f24c4dc6e4b0d85128213af25b85f93d394
dmytrobohdanov <dmytro.bohdanov@gmail.com>
2016-05-26 22:08:09 +0300

    refactored block with adding entry to phonebook

--- app/src/main/java/com/dmytro/bert/SettingsActivity.java
+++ app/src/main/java/com/dmytro/bert/SettingsActivity.java
@@ -15,52 +15,21 @@ import android.widget.ViewSwitcher;
  */
 public class SettingsActivity extends AppCompatActivity {
 
-    private Activity activity; //todo find another way
+    private final int VIEW_SWITCHER_ID = R.id.viewSwitcher;
+    private final int ADD_NEW_ENTRY_BUTTON_ID = R.id.addNewPhonenumber;
+    private final int ADD_TO_PHONEBOOK_SUBMIT_BUTTON_ID = R.id.addToPhoneBookButton;
+
+    private ViewSwitcher viewSwitcher;
+    private Button addNewEntryToPhonebook;
+    private Button submitAdding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        activity = this;
-
-        final ViewSwitcher viewSwitcher = (ViewSwitcher) this.findViewById(R.id.viewSwitcher);
-        Button addNewEntryToPhonebook = (Button) this.findViewById(R.id.addNewPhonenumber);
-        Button submitAdding = (Button) this.findViewById(R.id.addToPhoneBookButton);
-
-        assert submitAdding != null;
-        assert viewSwitcher != null;
-        assert addNewEntryToPhonebook != null;
-
-        addNewEntryToPhonebook.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //switching to add phone number form
-                //todo make it nicer
-                viewSwitcher.showNext();
-            }
-        });
-
-        submitAdding.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //todo inline all this
-
-                EditText nameView = (EditText) activity.findViewById(R.id.nameToAdd);
-                EditText phoneView = (EditText) activity.findViewById(R.id.phoneToAdd);
-
-                String name = nameView.getText().toString();
-                String phone = phoneView.getText().toString();
-
-                PhonesKeeper phonesKeeper = PhonesKeeper.getInstance(activity);
-                phonesKeeper.addPhoneNumber(name, phone);
-
-                viewSwitcher.showPrevious();
-                nameView.setText("");
-                phoneView.setText("");
-                Log.d("test", "size is" + phonesKeeper.getPhoneBookSize());
-            }
-        });
+        setAddPhoneNumberBlock(this);
 
 
         //should do:
@@ -79,5 +48,60 @@ public class SettingsActivity extends AppCompatActivity {
         // add triplet: emergency - text of notification - users to send
         //
         //
+    }
+
+
+    /**
+     * initialization of block
+     * which is responsible for adding new phone to phone book
+     * consist of ViewSwitcher
+     *
+     * @param activity setting activity
+     */
+    private void setAddPhoneNumberBlock(final Activity activity) {
+
+        viewSwitcher = (ViewSwitcher) this.findViewById(VIEW_SWITCHER_ID);
+        addNewEntryToPhonebook = (Button) this.findViewById(ADD_NEW_ENTRY_BUTTON_ID);
+        submitAdding = (Button) this.findViewById(ADD_TO_PHONEBOOK_SUBMIT_BUTTON_ID);
+
+
+        /**
+         * set onClick listener to addNewEntry button
+         * shows next from view switcher
+         * shows form to add phone number
+         */
+        addNewEntryToPhonebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //switching to add phone number form
+                viewSwitcher.showNext();
+            }
+        });
+
+        /**
+         * add onClick listener to submit button
+         * onClick: take name and phone number and adds it to phone book
+         */
+        submitAdding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo change ids to constants
+                EditText nameView = (EditText) activity.findViewById(R.id.nameToAdd);
+                EditText phoneView = (EditText) activity.findViewById(R.id.phoneToAdd);
+
+                String name = nameView.getText().toString();
+                String phone = phoneView.getText().toString();
+                //todo: validate name and phone fields
+
+                PhonesKeeper phonesKeeper = PhonesKeeper.getInstance(activity);
+                phonesKeeper.addPhoneNumber(name, phone);
+
+                viewSwitcher.showPrevious();
+                nameView.setText("");
+                phoneView.setText("");
+                Log.d("test", "size is" + phonesKeeper.getPhoneBookSize());
+            }
+        });
+
     }
 }
